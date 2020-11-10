147b9fa5680a896e0ca048618ddb1e438aa6b5e2
Marc Steele <marcsteele2014@gmail.com>
2020-11-10 15:47:59 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -10,7 +10,8 @@ module Players
     end
 
     def best_move(board)
-      win(board) || block(board) || corner(board) || random
+      # win(board) || block(board) ||
+       corner(board) || random
     end
 
     def win(board)
