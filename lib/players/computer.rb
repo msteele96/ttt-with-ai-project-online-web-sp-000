5dc931c5bea3f1ba6d2da5948e55856859fd8c39
Marc Steele <marcsteele2014@gmail.com>
2020-11-10 03:20:21 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,10 +2,11 @@ module Players
   class Computer < Player
 
     def move(board)
+      corners = [1,3,7,9]
       if board.taken?("5") == false
-        5
-      elsif board.taken?("1") || board.taken?("3") || board.taken?("7") || board.taken?("9")
-
+        "5"
+      else
+        (1..9).to_a.sample
       end
     end
 
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -2,6 +2,7 @@ module Players
   class Human < Player
 
     def move(board)
+      "Where would you like to go? (1-9)"
       gets.strip
     end
 
