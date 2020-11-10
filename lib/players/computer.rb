1ff7330ec53749d99f051b37d7b4dea3485932a7
Marc Steele <marcsteele2014@gmail.com>
2020-11-10 16:21:00 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,12 +2,24 @@ module Players
   class Computer < Player
 
     def move(board)
-      corners = [1,3,7,9]
       if board.taken?("5") == false
         "5"
-      else
-        (1..9).to_a.sample
+      elsif board.taken?("5")
+        corner(board) || random
       end
+    end
+
+    def corner(board)
+      corners = [1,3,7,9]
+      corners.find { |corner| board.taken?(corner) == false }
+    end
+
+    def random
+      (1..9).to_a.sample
+    end
+
+    def opponent_token
+      self.token == "X" ? "O" : "X"
     end
 
   end
